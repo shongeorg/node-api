a9fe842d7fa6fdebfad445ef1c26ac2ed15469f7
shongeorg <shongeorg48@gmail.com>
2024-10-13 18:44:09 +0300

    add routes

--- server.mjs
+++ server.mjs
@@ -1,8 +1,54 @@
 import { createServer } from "node:http";
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ name: "nodejs" }));
+const data = {
+  posts: [
+    { name: "nodejs", id: 1 },
+    { name: "npm", id: 2 },
+    { name: "react", id: 3 },
+  ],
+};
+const toJson = (data) => JSON.stringify(data);
+
+const routes = {
+  "/": async () => {
+    return toJson(data);
+  },
+  posts: async (id) => {
+    const post = data.posts.find((x) => x.id === +id);
+    return toJson(post);
+  },
+};
+
+const getContent = async (pathname) => {
+  const [, route, param] = pathname.split("/");
+
+  if (route === "" && routes["/"]) {
+    return await routes["/"]();
+  }
+
+  if (routes[route]) {
+    return await routes[route](param);
+  }
+
+  throw new Error("404");
+};
+
+const server = createServer(async (req, res) => {
+  const url = req.url ? new URL(req.url, `http://${req.headers.host}`) : null;
+  try {
+    const content = await getContent(url?.pathname || "/");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(content);
+  } catch (error) {
+    if (error.message === "404") {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end("<h1>404 Not Found</h1>");
+    } else {
+      console.error("Error:", error);
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<h1>500 Internal Server Error</h1>");
+    }
+  }
 });
 
 server.listen(3000, "127.0.0.1", () => {
